6f82a56b5a9851bbadb4b8f483f91d1f66b4d688
joaoleite2 <joao.cont3t@gmail.com>
2024-04-29 20:38:30 -0300

    feat: prisma and models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BrandModule } from './auto/brand/brand.module';
+import { ModelModule } from './auto/model/model.module';
+import { ProductTypeModule } from './auto/product-type/product-type.module';
+import { ProductModule } from './auto/product/product.module';
 
 @Module({
-  imports: [],
+  imports: [BrandModule, ModelModule, ProductTypeModule, ProductModule],
   controllers: [],
   providers: [],
 })
