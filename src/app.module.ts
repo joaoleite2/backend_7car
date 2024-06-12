2009d3123dc9de6be81e5bd88b52119cddec01a2
joaoleite2 <joao.cont3t@gmail.com>
2024-06-12 18:23:00 -0300

    feat: cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,12 @@ import { ModelModule } from './auto/model/model.module';
 import { ProductTypeModule } from './auto/product-type/product-type.module';
 import { ProductModule } from './auto/product/product.module';
 import { SaleModule } from './sale/sale.module';
-import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { config } from 'dotenv';
 config();
 
 @Module({
-  imports: [BrandModule, ModelModule, ProductTypeModule, ProductModule, SaleModule, AuthModule, UserModule],
+  imports: [BrandModule, ModelModule, ProductTypeModule, ProductModule, SaleModule, UserModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(8080);
 }
 bootstrap();
